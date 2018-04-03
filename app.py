a5cb282f18b6836a2b8cc7569a137232a17c640f
Pasoon Azimi <pasoon.azimi10@gmail.com>
2018-04-02 23:12:54 -0400

    Added models for restaurant and rating

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
-from flask import Flask
+
+from flask import Flask, request, redirect, url_for
+from flask import render_template
 from models import *
+
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://zshah011:University-25@web0.site.uottawa.ca:15432/zshah011'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # silence the deprecation warning
 
 db.app = app
 db.init_app(app)
 
 db.create_all()
 
+
+
+
 @app.route('/')
 def index():
-    return 'hello flask'
+    return render_template('post_resto.html')
 
 @app.route('/post_resto', methods=['POST'])
 def post_resto():
-    return
+    restaurant = Restaurant(request.form['name'], request.form['type'], request.form['url'], request.form['rating'])
+    db.session.add(restaurant)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -20,4 +20,41 @@ class Rater(db.Model):
         return '<Rater %r>' % self.userid
 
 
+class Restaurant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), unique=True)
+    type = db.Column(db.String(), unique=False)
+    url = db.Column(db.String(), unique=True)
+    rating = db.Column(db.Integer, unique=False)
 
+    def __init__(self, name, type, url, rating):
+        self.name = name
+        self.type = type
+        self.url = url
+        self.rating = rating
+
+    def __repr__(self):
+        return '<Restaurant %r>' % self.name
+
+
+class Rating(db.Model):
+    userid = db.Column(db.Integer, primary_key=True)
+    postdate = db.Column(db.String(), primary_key=True)
+    restaurantId = db.Column(db.Integer, primary_key=True)
+    pricerating = db.Column(db.Integer, unique=False)
+    foodrating = db.Column(db.Integer, unique=False)
+    moodrating = db.Column(db.Integer, unique=False)
+    staffrating = db.Column(db.Integer, unique=False)
+    comment = db.Column(db.String(), unique=False)
+
+    def __init__(self, userid, postdate, restaurantId, pricerating, foodrating, moodrating, staffrating, comment):
+        self.userid = userid
+        self.postdate = postdate
+        self.restaurantId = restaurantId
+        self.pricerating = pricerating
+        self.foodrating = foodrating
+        self.moodrating = moodrating
+        self.staffrating = staffrating
+
+    def __repr__(self):
+        return '<Rating: %r>' % self.userid
